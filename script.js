7ce91fe1a8c98494ae52907c225fae77d81875f1
Tamal Chakraborty <ckbtamaldipnew@gmail.com>
2024-01-05 17:26:14 +0600

    added the edit functionality

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const input = document.querySelector('.input');
 const addButton = document.querySelector('.addBtn');
 const ul = document.querySelector('.ul');
 
+//This function set the cursor in the end of leble when we click the edit button 
+const setCursorToEnd = (element) => {
+    var range = document.createRange();
+    var selection = window.getSelection();
+    
+    range.selectNodeContents(element);
+    range.collapse(false); // Collapse range to the end
+    selection.removeAllRanges();
+    selection.addRange(range);
+}
 
 const TodoCreator = (todo) =>{
 
@@ -42,7 +52,7 @@ const TodoCreator = (todo) =>{
     button1.classList.add('btn','edit');
     button2.classList.add('btn','cancel');
 
-    button1.innerText = "edit";
+    button1.innerText = "Edit";
     button2.innerText = "X";
 
     buttonDiv.append(button1);
@@ -64,18 +74,31 @@ addButton.addEventListener('click', (e)=>{
     if(todo){
         TodoCreator(todo);
     }else{
-        alert("Enter some text first")
+        alert("Enter some text first");
     }
     input.value = "";
 
 })
 
-//delete
+//Update, cancel and check with event delegation 
 ul.addEventListener('click', (e)=>{
     if(e.target.innerText === 'X'){
         const element = e.target;
         element.parentElement.parentElement.remove();
+    }else if(e.target.innerText === 'Edit'){
+        const labelText = e.target.parentElement.previousElementSibling.children[1];
+        labelText.setAttribute('contenteditable', 'true')
+        e.target.innerText = "Done";
+        labelText.focus();
+        setCursorToEnd(labelText);
+    }else if(e.target.innerText === "Done"){
+        const labelText = e.target.parentElement.previousElementSibling.children[1];
+        if(labelText.innerText){
+            labelText.setAttribute('contenteditable', 'false')
+            e.target.innerText = "Edit";
+        }else{
+            alert("Enter some text first");
+            labelText.focus();
+        }
     }
-    console.log(e.target.innerText);
-
 })
